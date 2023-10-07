d760db50c766e9fc308e7cde0f3869afc3f5b44f
albfernandes <albfernandes@hotmail.com>
2023-10-07 10:28:37 -0300

    "primeiro commit"

---
+++ src/EstruturaComposta/NotaAluno.java
@@ -0,0 +1,14 @@
+package EstruturaComposta;
+
+public class NotaAluno {
+    public static void main(String[] args) {
+
+       int nota = 6;
+       
+       if(nota >= 7)
+        System.out.println("Aprovado");
+
+       else
+        System.out.println("Reprovado");
+    }
+}
